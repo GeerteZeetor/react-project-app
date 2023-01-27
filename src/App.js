c5197a5b8fdbe466443053394ff2590202c00378
AndreyMorozov <pisemest@yandex.ru>
2023-01-27 21:12:25 +0500

    add login1

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <h2 className="textHeader">Todo App</h2>
       <TodoForm addTodo={addTodoHandler} />
       <TodoList todos={todos} />
+        <div>Login1</div>
     </div>
   );
 }
