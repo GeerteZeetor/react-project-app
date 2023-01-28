18d2d1299056eb7c00c74e078b880408eafea933
AndreyMorozov <pisemest@yandex.ru>
2023-01-28 15:47:46 +0500

    add icon and google font

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,9 +1,13 @@
 import styles from './Todo.module.css';
+import { RiTodoFill } from 'react-icons/ri';
 
 export default function Todo({ todo, index, deleteTodo }) {
   return (
     <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
-      <h4 className={styles.textTodo}>{todo}</h4>
+      <h3 className={styles.textTodo}>
+        <RiTodoFill className={styles.todoIcon} />
+        {todo}
+      </h3>
     </div>
   );
 }
