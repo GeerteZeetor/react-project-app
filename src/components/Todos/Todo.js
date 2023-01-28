c7c860954bdd9a69c941ad62ecbdc4549570d48d
AndreyMorozov <pisemest@yandex.ru>
2023-01-28 14:57:04 +0500

    h3 to h4

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,4 +1,4 @@
 import styles from './Todo.module.css';
 export default function Todo({ todo }) {
-  return <h3 className={styles.textTodo}>{todo}</h3>;
+  return <h4 className={styles.textTodo}>{todo}</h4>;
 }
