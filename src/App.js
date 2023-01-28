0dd4f2c8ee05a1f90ad0e13a929ea4ecae40522f
AndreyMorozov <pisemest@yandex.ru>
2023-01-28 14:55:43 +0500

    change h2 to h1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <h2 className="textHeader">Todo App</h2>
+      <h1 className="">Todo App</h1>
       <TodoForm addTodo={addTodoHandler} />
       <TodoList todos={todos} />
     </div>
